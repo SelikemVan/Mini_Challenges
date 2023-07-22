b767561a43bdfd92b45f8bd6df3ae8d3d23bbf6b
SelikemVan <selikemvan@gmail.com>
2023-07-22 11:31:50 +0100

    commit for Mini Challenges

---
+++ Mini_Challenges_for_5th_file.py
@@ -0,0 +1,8 @@
+# MINI CHALLENGE #1:
+
+# Write a code that takes in 2 lists
+# x = [-1 -4 -7] and y = [-3 7 4] and calculates the absolute sum of all its elements |x+y|
+
+x = [-1-4-7]
+y = [-3,7,4]
+print(sum(x + y))
